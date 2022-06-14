4ff0e5c32cd1d3cff983d9e91d7c848fcb42902e
miguel-uicab <miguel.uicab.perera@gmail.com>
2022-06-14 18:19:38 -0500

    Añade tests

---
+++ V5/tests/unit/extras.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+
+def get_body():
+    """read event.json and return body"""
+    with open('events/event.json') as file:
+        event = json.load(file)
+    return event["body"]
+
+
+def get_environments():
+    """read environments.json and return dict"""
+    with open(f'environments.json') as file:
+        environments = json.load(file)
+
+    return environments["SuggestedPriceV5Function"]
